29bcf7581f37271b45173e9ea8d998d2394d0df0
Martin Deinhofer <deinhofe@technikum-wien.at>
2018-09-27 12:38:58 +0200

    set default prefix to m_

--- studyathome/start-all.sh
+++ studyathome/start-all.sh
@@ -2,7 +2,7 @@
 
 export MOODLE_DOCKER_WWWROOT="$(pwd)/../moodle"
 export MOODLE_DOCKER_DB="pgsql"
-export MOODLE_DOCKER_DBPREFIX="UniA_m_"
+export MOODLE_DOCKER_DBPREFIX="m_"
 export MOODLE_DOCKER_DB_VOLUME="$(pwd)/studyathome/db-volume"
 export MOODLE_DOCKER_DATAROOT="$(pwd)/studyathome/UniA/moodledata"
 
